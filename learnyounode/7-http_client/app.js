e4112193de3e622eb330b84fb17dd2599c1270d4
Silvia <silvi603@gmail.com>
2014-06-10 20:14:02 +0200

    learnyounode example 7

---
+++ learnyounode/7-http_client/app.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var url = process.argv[2];
+
+http.get(url, function(res) {
+	res.setEncoding('utf-8');
+
+	res.on('data', function(data) {
+		console.log(data);
+	})
+
+	res.on('error', function(err) {
+		console.error(err);
+	})
+})
